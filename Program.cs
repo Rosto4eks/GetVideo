51004e0400ec8bad48d84f3c31a87a7f2f4bf824
Rosto4eks <rosto4eks@mail.ru>
2022-07-15 20:14:04 +0300

    v2, this version much faster than first one

--- Program.cs
+++ Program.cs
@@ -1,94 +1,99 @@
-﻿using VideoLibrary;
+﻿using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+using YoutubeExplode.Converter;
 using System.Diagnostics;
-using FFMpegCore;
 
 class GetVideo {
-    static void Main() {    
+
+    static async Task Main() {
+        var youtube = new YoutubeClient();
         Console.Write("Paste the link: ");
         string? link = Console.ReadLine();
-        if (!Directory.Exists(@"downloads\parts")) Directory.CreateDirectory(@"downloads\parts");
-        DirectoryInfo directory = new DirectoryInfo(@".\downloads\");
-        YouTube youtube = new YouTube();
-        IEnumerable<YouTubeVideo> videos = youtube.GetAllVideos(link);
-        ChooseQuality(directory, videos);
+        if (link != null) { 
+            try {
+                Console.Clear();
+                await CheckLink(link, youtube);
+            }
+            catch (Exception error) {
+                Console.ResetColor();
+                Console.CursorVisible = true;
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(error.Message);
+                Console.ResetColor();
+                await Main();
+            }
+        }
+        Process.Start("explorer", "downloads");
+        Console.Clear();
+        Console.Write("Continue? (Y/N): ");
+        string? response = Console.ReadLine()?.ToUpper();
+        if (response == "Y") {
+            Console.Clear();
+            await Main();
+        }
+        Process.GetCurrentProcess().Kill();
     }
 
-    static List<int> GetQualities(IEnumerable<YouTubeVideo> videos) {
-        List<int> qualities = new List<int>();
-        foreach (YouTubeVideo video in videos) {
-            if (!qualities.Contains(video.Resolution) && video.Resolution >= 144) qualities.Add(video.Resolution);
-        }
-        List<int> sorted = qualities.OrderByDescending(e => e).ToList();
-        return sorted;
+    static async Task CheckLink(string link, YoutubeClient youtube) {
+        var streamManifest = await youtube.Videos.Streams.GetManifestAsync(link);
+        var video = await youtube.Videos.GetAsync(link);
+        await GetQualities(streamManifest, youtube, video.Title);
     }
 
-    static void ChooseQuality(DirectoryInfo directory, IEnumerable<YouTubeVideo> videos) {
-        List<int> qualities = GetQualities(videos);
-        Console.WriteLine("Choose the quality:");
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        for (int e = 0; e < qualities.Count; e++) {
-            Console.WriteLine($"{e + 1} - {qualities[e]}");
+    static async Task GetQualities(StreamManifest streamManifest, YoutubeClient youtube, string name) {
+        var videoStreams = streamManifest.GetVideoStreams();
+        var qualities = new List<string>();
+        foreach (var videoStream in videoStreams) {
+            if (!qualities.Contains(videoStream.VideoQuality.Label)) {
+                qualities.Add(videoStream.VideoQuality.Label);
+            }
         }
-        Console.ResetColor();
-        Console.Write("Enter the number: ");
-        int quality = 1;
-        try {
-             quality = Convert.ToInt32(Console.ReadLine());
+        var sortedQualities = qualities.OrderByDescending(e => Convert.ToInt32(e.Replace("p", ""))).ToList();
+        Console.Clear();
+        await GetQuality(sortedQualities, name, streamManifest, youtube);
+    }
+
+    static async Task GetQuality(List<string> qualitites, string name, StreamManifest streamManifest, YoutubeClient youtube) {
+        for (int count = 0; count < qualitites.Count; count++) {
+            Console.WriteLine($"{count+1} - {qualitites[count]}");
         }
-        catch {
+        Console.Write("Choose quality: ");
+        int quality = Convert.ToInt32(Console.ReadLine());
+        if (quality < 1 || quality > qualitites.Count) {
+            Console.Clear();
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Enter the correct number");
+            Console.WriteLine("Invalid number");
             Console.ResetColor();
-            Thread.Sleep(1000);
-            ChooseQuality(directory, videos);
+            await GetQuality(qualitites, name, streamManifest, youtube);
         }
-        if (quality < 1 || quality > qualities.Count) {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Enter the correct number");
-            Console.ResetColor();
-            Thread.Sleep(1000);      
-            ChooseQuality(directory, videos);
-        }
-        Console.WriteLine($"Choosed quality: {qualities[quality - 1]}");
-        YouTubeVideo video = videos.Where(e =>
-            e.Format == VideoFormat.Mp4 && 
-            e.Resolution == qualities[quality-1] && 
-            e.AudioFormat == AudioFormat.Unknown
-        ).First();
-        Download(videos, directory, video);
+        Console.Clear();
+        await Download(qualitites[quality-1], name, streamManifest, youtube);
     }
 
-    static YouTubeVideo GetAudio(IEnumerable<YouTubeVideo> videos) {
-        YouTubeVideo video = videos.Where(e => e.AdaptiveKind == AdaptiveKind.Audio).First();
-        return video;
+    static async Task Download(string quality, string name, StreamManifest streamManifest, YoutubeClient youtube) {
+        var videoStreamInfo = streamManifest.GetVideoStreams().First(e => e.VideoQuality.Label == quality);
+        var audioStreamInfo = streamManifest.GetAudioStreams().GetWithHighestBitrate();
+        var streamInfos = new IStreamInfo[] { videoStreamInfo, audioStreamInfo };
+        if (!Directory.Exists(@"downloads")) Directory.CreateDirectory(@"downloads");
+        Console.CursorVisible = false;
+        Console.Write("Progress: ");
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.Write("                                                                                                    ");
+        Console.BackgroundColor = ConsoleColor.Green;
+        Console.SetCursorPosition(10, 0);
+        int lastPercent = 0;
+        var progress = new Progress<double>(p => {
+            var percent = Convert.ToInt32(p * 100);;
+            if (percent != lastPercent) {
+                Console.Write(" ");
+                lastPercent = percent;
+            }
+            if (percent == 100) {
+                Console.ResetColor();
+                Console.CursorVisible = true;
+            }
+        });
+        await youtube.Videos.DownloadAsync(streamInfos, new ConversionRequestBuilder($@"downloads\{name}.mp4").Build(), progress);
     }
-
-    static void Download(IEnumerable<YouTubeVideo> videos, DirectoryInfo directory, YouTubeVideo video) {
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("Downloading video...");
-        File.WriteAllBytes($@"{directory}parts\{video.FullName}", video.GetBytes());
-        YouTubeVideo audio = GetAudio(videos);
-        Console.WriteLine("Downloading audio...");
-        File.WriteAllBytes($@"{directory}parts\{audio.FullName}.mp3", audio.GetBytes());
-        Console.WriteLine("merging video and audio...");
-        Console.ResetColor();
-        FFMpeg.ReplaceAudio($@"{directory}parts\{video.FullName}", $@"{directory}parts\{audio.FullName}.mp3", $"{directory}{video.FullName}");
-        File.Delete($@"{directory}parts\{video.FullName}");
-        File.Delete($@"{directory}parts\{audio.FullName}.mp3");
-        Console.WriteLine("Complete");
-        Continue();
-    }
-
-    static void Continue() {
-        Console.WriteLine("continue? (Y/N)");
-        string? answer = Console.ReadLine()?.ToUpper();
-        if (answer == "Y") Main();
-        else if (answer == "N") Process.GetCurrentProcess().Kill();
-        else {
-            Console.WriteLine("Incorrect input");
-            Thread.Sleep(1000);
-            Continue();
-        }
-    }
-
 }
